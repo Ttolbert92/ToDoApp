05da2d9c9fa3cf185a9b1ee042a91be3f40c732b
Tamika Tolbert <beatty.tamika@gmail.com>
2023-05-29 19:33:55 -0700

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Deangelo Tolbert on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
